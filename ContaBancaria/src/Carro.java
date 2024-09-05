e01fd2b99b51ddb9a4815b179f821f82ed440769
Lucas Mateus <Lucas015x@gmail.com>
2024-09-04 21:17:36 -0300

    feat: Crie uma classe de carro para fixar o conhecimento, mais ainda terá mais implementações

---
+++ ContaBancaria/src/Carro.java
@@ -0,0 +1,26 @@
+public class Carro {
+    String cor;
+    String modelo;
+    double velocidadeAtual;
+    double velocidadeMaxima;
+
+    void ligar() {
+        System.out.println("O carro está ligado!");
+    }
+
+    void acelerar(double quantidade) {
+        double velocidadeNova = this.velocidadeAtual + quantidade;
+        this.velocidadeAtual = velocidadeNova;
+    }
+
+    int pegarMarcha() {
+        if (this.velocidadeAtual < 0)
+            return -1;
+        if (this.velocidadeAtual >= 0 && this.velocidadeAtual < 40)
+            return 1;
+        if (this.velocidadeAtual >= 40 && this.velocidadeAtual < 80)
+            return 2;
+
+        return 3;
+    }
+}
---
+++ ContaBancaria/src/TestarCarro.java
@@ -0,0 +1,15 @@
+public class TestarCarro {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+
+        meuCarro.cor = "Preto";
+        meuCarro.modelo = "Golf GTR";
+        meuCarro.velocidadeAtual = 0;
+        meuCarro.velocidadeMaxima = 210;
+
+        meuCarro.ligar();
+
+        meuCarro.acelerar(20);
+        System.out.println("A velocidade atual do seu carro é: " + meuCarro.velocidadeAtual + "KM/H");
+    }
+}
